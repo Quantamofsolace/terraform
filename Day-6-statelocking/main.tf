fd370cd061f894e70ef9010995e2ca1f131c4b39
Kumar <saltandpepperforaws@gmail.com>
2026-03-13 23:43:14 +0530

    day6modifiedwithstatelocking

--- Day-6-statelocking/main.tf
+++ Day-6-statelocking/main.tf
@@ -2,14 +2,14 @@ resource "aws_instance" "name" {
     ami           = "ami-0f559c3642608c138"
     instance_type = "t3.micro"
     tags = {
-        Name = "quantamofsoalce-Instance"
+        Name = "quantamofsoalce-hello"
     }
   
 }
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
-        Name = "ewwwwww-VPC"
+        Name = "ewwwwww-haha-VPC"
     }
 }
 
@@ -17,6 +17,6 @@ resource "aws_subnet" "name" {
     vpc_id     = aws_vpc.name.id
     cidr_block = "10.0.1.0/24"
     tags = {
-        Name = "ewwwwww-Subnet"
+        Name = "ewwwwww-haha-Subnet"
     }
 }
