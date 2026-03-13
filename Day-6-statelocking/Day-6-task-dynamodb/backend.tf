e90687320be86688bc6f5d3564bae409a649cf11
Kumar <saltandpepperforaws@gmail.com>
2026-03-14 00:12:06 +0530

    day6changesinthebacked.tf

--- Day-6-statelocking/Day-6-task-dynamodb/backend.tf
+++ Day-6-statelocking/Day-6-task-dynamodb/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "veeranareshitdev"
-    key    = "day-4/terraform.tfstate"
-    region = "us-east-1"
+    bucket = "feelinggoodmix"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
     # Enable S3 native locking
-    #use_lockfile = true   #tf version should be above 1.10
+    use_lockfile = true   #tf version should be above 1.10
     # The dynamodb_table argument is no longer needed
     dynamodb_table = "terraform-state-lock-dynamo"
 
