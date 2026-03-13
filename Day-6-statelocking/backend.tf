bf0c6114776a4d85bc0d28e7384ab1eea2f2123c
Kumar <saltandpepperforaws@gmail.com>
2026-03-13 23:30:23 +0530

    daystatelockuselockfiletrue

--- Day-6-statelocking/backend.tf
+++ Day-6-statelocking/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "feelinggoodmix"
     key    = "terraform.tfstate"
     region = "ap-south-1"
+    use_lockfile = true #dynamodb no longer required for state locking in s3 backed we can use lockfile.
+    #terraform version should be 1.10 and above to use the lockfile for state locking in s3 backend.
   }
 }
